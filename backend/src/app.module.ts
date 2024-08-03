a62bfbf7c2881b9a0b59189bd3083bc27e9064d1
An Beed <an@getbeed.com>
2024-08-04 03:30:14 +0700

    fix: missing usermodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './modules/auth/module/auth.module';
+import { UserModule } from './modules/user/module/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { AuthModule } from './modules/auth/module/auth.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI || ''),
     AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
