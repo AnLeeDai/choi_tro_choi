ff2031c8784187e2f81e6865ace86ee662cfe2e2
Lê Đại An <169076347+AnBeed@users.noreply.github.com>
2024-08-04 13:02:39 +0700

    feat: init source code front end (#2)

--- frontend/app/(home)/views/home-container.tsx
+++
@@ -1,9 +0,0 @@
-import ButtonSetCount from "./button-set-count";
-
-export default function HomeContainer() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen gap-3">
-      <ButtonSetCount />
-    </div>
-  );
-}
--- frontend/app/(home)/views/show-count.tsx
+++
@@ -1,5 +0,0 @@
-function ShowCount({ count }: { count: number }) {
-  return <p>count: {count}</p>;
-}
-
-export default ShowCount;
